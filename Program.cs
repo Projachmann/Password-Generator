8f07b203f8e74ef2fff6d0348ed676f87e178008
Projachmann <pro.jachmann@gmail.com>
2024-07-27 21:02:14 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Password_Generator
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string baseUrl = "https://api.api-ninjas.com/v1/passwordgenerator";
+            int passwordLength = 10;
+            string url = $"{baseUrl}?length={passwordLength}";
+
+            using (HttpClient client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Add("X-Api-Key", "YOUR_API_KEY");
+
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    string responseBody = await response.Content.ReadAsStringAsync();
+                    var passwordResponse = JsonConvert.DeserializeObject<PasswordResponse>(responseBody);
+
+                    Console.WriteLine($"Generated Password: {passwordResponse.Password}");
+                }
+                catch (HttpRequestException e)
+                {
+                    Console.WriteLine($"Request error: {e.Message}");
+                }
+            }
+        }
+    }
+
+    public class PasswordResponse
+    {
+        [JsonProperty("password")]
+        public string Password { get; set; }
+    }
+}
